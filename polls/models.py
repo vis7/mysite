5748ff41c79108b3ed6ab224ead20e6a8e527bc0
vis7 <vishvajeetramanuj95@gmail.com>
2022-01-11 12:21:55 +0530

    added code for test signals

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
+
 
 # Create your models here.
 class Question(models.Model):
@@ -17,6 +20,14 @@ class Question(models.Model):
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = "Published Recently ?"
+
+@receiver(pre_save, sender=Question)
+def demo_pre_save(sender, **kwargs):
+	print("this function will be called before question is actually saved")
+
+@receiver(post_save, sender=Question)
+def demo_post_save(sender, **kwargs):
+	print("this signal will be called after saving the model")
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
